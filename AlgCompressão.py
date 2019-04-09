e4f4b9a2803319cfd580ed75421ae55af0ee506a
Luis Coelho <luis.fs.coelho@pm.me>
2019-04-09 11:45:19 -0300

    Algoritmo de Compressao

---
+++ AlgCompressão.py
@@ -0,0 +1,26 @@
+
+# ******  TABELA    ********
+# |  A  |  C  |  T  |  G   |
+# |  1  | 000 | 001 |  01  |
+# --------------------------
+
+A = '1'
+C = '000'
+T = '001'
+G = '01'
+
+s = input()
+sn = s.replace('A', A).replace('C', C).replace('T', T ).replace('G', G)
+
+print('{} {:.1f}'.format(sn, 100 - ((len(sn) * 100) / (len(s) * 8 ))))  
+
+# 100 - (((len(ns)*100)/(len(s)*8))
+
+# Ex: ATC (24bits) - 1001000 (7bits)
+# Fórmula:  100 - ((7x100)/24)
+# Entrada: ATC
+# Saída: 1001000 70.8
+
+# Teste
+# E ACTGACTGACTGACTGACTGACTG
+# S 100000101100000101100000101100000101100000101100000101 71.9
