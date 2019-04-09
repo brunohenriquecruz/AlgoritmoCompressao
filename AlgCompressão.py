3bea7e229fa6c946f7769931e3983e2b5bc97f61
brunohenriquecruz <brunohenriquecruz@yahoo.com.br>
2019-04-09 11:52:11 -0300

    variavel

--- AlgCompressão.py
+++ AlgCompressão.py
@@ -9,10 +9,10 @@ C = '000'
 T = '001'
 G = '01'
 
-s = input()
-sn = s.replace('A', A).replace('C', C).replace('T', T ).replace('G', G)
+ent = input()
+entS = ent.replace('A', A).replace('C', C).replace('T', T ).replace('G', G)
 
-print('{} {:.1f}'.format(sn, 100 - ((len(sn) * 100) / (len(s) * 8 ))))  
+print('{} {:.1f}'.format(entS, 100 - ((len(sn) * 100) / (len(ent) * 8 ))))  
 
 # 100 - (((len(ns)*100)/(len(s)*8))
 
